d1ae7bb1c4e8ac98eb9ee54f5a81fc1422ad77d5
Amuh Emeka <99865958+jeffykings@users.noreply.github.com>
2023-12-22 08:18:35 -0800

    RSA Challenge

---
+++ rsa.c
@@ -0,0 +1,33 @@
+#include "factor.h"
+
+/**
+ * main - main function
+ *
+ * Author: Thaoban Abdrasheed
+ * Return: void
+ */
+int main(int argc, char *argv[])
+{
+	FILE *fp;
+	size_t cunt;
+	ssize_t ln;
+	char *buffer = NULL;
+
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: factor <filename>\n");
+		exit(EXIT_FAILURE);
+	}
+	fp = fopen(argv[1], "r");
+	if (fp == NULL)
+	{
+		fprintf(stderr, "Error: can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while((ln = getline(&buffer, &cunt, fp)) != -1)
+	{
+		fctrze(buffer);
+	}
+return (0);
+}
